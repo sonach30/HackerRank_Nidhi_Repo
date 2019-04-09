d63bba812e8d0cd0a855c7764a99e033dab8f65e
Nidhi Chaubey <nidhich30@gmail.com>
2019-04-09 14:46:20 +0530

    Added the code

--- Hello,World!.js
+++ Hello,World!.js
@@ -2,7 +2,7 @@ function greeting(parameterVariable) {
     // This line prints 'Hello, World!' to the console:
     console.log('Hello, World!');
 
-    console.log('Welcome to 10 Days of JavaScript!');
+    console.log(parameterVariable);
     // Write a line of code that prints parameterVariable to stdout using console.log:
     
 }
