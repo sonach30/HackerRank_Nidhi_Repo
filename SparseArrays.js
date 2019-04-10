d0fabd448a02db1e8dec4045c1b2f80f86f0460a
Nidhi Chaubey <nidhich30@gmail.com>
2019-04-10 12:48:08 +0530

    Added solution for Sparse Arrays

---
+++ SparseArrays.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the matchingStrings function below.
+function matchingStrings(strings, queries) {
+    var sindex;
+    var qindex;
+    var count;
+    var slen = strings.length;
+    var qlen = queries.length;
+    var a = [];
+    var i = 0;
+
+    for (qindex = 0; qindex < qlen; qindex++) {
+        count = 0;
+        for (sindex = 0; sindex < slen; sindex++) {
+            if (queries[qindex].localeCompare(strings[sindex]) == 0) {
+
+                count++;
+            }
+        }
+        a[i] = count;
+        i++;
+    }
+    return a;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const stringsCount = parseInt(readLine(), 10);
+
+    let strings = [];
+
+    for (let i = 0; i < stringsCount; i++) {
+        const stringsItem = readLine();
+        strings.push(stringsItem);
+    }
+
+    const queriesCount = parseInt(readLine(), 10);
+
+    let queries = [];
+
+    for (let i = 0; i < queriesCount; i++) {
+        const queriesItem = readLine();
+        queries.push(queriesItem);
+    }
+
+    let res = matchingStrings(strings, queries);
+
+    ws.write(res.join("\n") + "\n");
+
+    ws.end();
+}
