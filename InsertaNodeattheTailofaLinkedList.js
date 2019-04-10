c8033c04a7476a27a558ce381f909b63ad10393b
Nidhi Chaubey <nidhich30@gmail.com>
2019-04-10 14:04:17 +0530

    Added solution for Insert a Node at the Tail of a Linked List

---
+++ InsertaNodeattheTailofaLinkedList.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+    }
+
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the insertNodeAtTail function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function insertNodeAtTail(head, data) {
+
+    let newNode = new SinglyLinkedList();
+    let node = new SinglyLinkedList();
+    let node2 = new SinglyLinkedList();
+    newNode.data = data;
+    newNode.next = null;
+    node = head;
+    node2 = head;
+    
+    
+    while (node != null) {
+
+        node=node.next;
+    }
+
+    node = newNode;
+ 
+    return head;
+}
+
+function main() {
