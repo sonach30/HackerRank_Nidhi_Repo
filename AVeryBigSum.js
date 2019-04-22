45645d8d1402b29fad762db36cdfaa96928c3057
Nidhi Chaubey <nidhich30@gmail.com>
2019-04-22 16:54:32 +0530

    A Very Big Sum

--- AVeryBigSum.js
+++ AVeryBigSum.js
@@ -12,7 +12,7 @@ process.stdin.on('data', inputStdin => {
     inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
+process.stdin.on('end', function() {
     inputString = inputString.replace(/\s*$/, '')
         .split('\n')
         .map(str => str.replace(/\s*$/, ''));
@@ -24,26 +24,50 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-// Complete the aVeryBigSum function below.
-function aVeryBigSum(ar) {
-    var sum=0, i;
-    var len = ar.length;
+// Complete the diagonalDifference function below.
+function diagonalDifference(arr) {
+    var len = arr.length;
+    //console.log(len);
+    var sum1=0, sum2=0;
+    var len = arr.length;
+    var i, j;
     for (i = 0; i < len; i++) {
-        sum = sum + ar[i];
+        for (j = 0; j < len; j++) {
+            if (i == j) {
+                sum1 = sum1 + arr[i][j];
+            }
+        }
     }
-    return sum;
+        for (i = 0; i < len; i++) {
+            for (j = len - 1; j >= 0; j--) {
+                //console.log(j + " " + (len - 1));
+                if (i == Math.abs(j - (len - 1))) {
+                    sum2 = sum2 + arr[i][j];
+                    console.log(sum2);
+                }
+            }
+        }
+            console.log(sum1 + " " + sum2);
+            return Math.abs(sum1 - sum2);
+        
+         
+    
 }
 
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const arCount = parseInt(readLine(), 10);
+    const n = parseInt(readLine(), 10);
 
-    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+    let arr = Array(n);
 
-    let result = aVeryBigSum(ar);
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
 
-    ws.write(result + "\n");
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
 
     ws.end();
 }
