ea53effbb9bed474318dbe7437eb55ae189ceee6
Nidhi Chaubey <nidhich30@gmail.com>
2019-04-22 16:56:25 +0530

    check BST

---
+++ checkBST.java
@@ -0,0 +1,25 @@
+/* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.  
+
+The Node class is defined as follows:
+    class Node {
+    int data;
+    Node left;
+    Node right;
+     }
+*/
+    boolean checkBST(Node root) {
+        return isBSTUtil(root, Integer.MIN_VALUE, 
+                               Integer.MAX_VALUE); 
+    } 
+  
+    boolean isBSTUtil(Node node, int min, int max) 
+    { 
+        if (node == null) 
+            return true; 
+  
+        if (node.data < min || node.data > max) 
+            return false; 
+ 
+        return (isBSTUtil(node.left, min, node.data-1) && 
+                isBSTUtil(node.right, node.data+1, max)); 
+    }
